aa95fba595f9936e43e75b670831536108a62f96
S1532011 <s1532011@student.mcckc.edu>
2023-03-29 13:09:45 -0500

    Chapter 8 Assignment

--- ch08_ex1/index.php
+++ ch08_ex1/index.php
@@ -3,22 +3,34 @@
 $customer_type = filter_input(INPUT_POST, 'type');
 $invoice_subtotal = filter_input(INPUT_POST, 'subtotal');
 
-if ($customer_type == 'R') {
-    if ($invoice_subtotal < 100) {
-        $discount_percent = .0;
-    } else if ($invoice_subtotal >= 100 && $invoice_subtotal < 250) {
-        $discount_percent = .1;
-    } else if ($invoice_subtotal >= 250) {
-        $discount_percent = .25;
-    }
-} else if ($customer_type == 'C') {
-    if ($invoice_subtotal < 250) {
+switch($customer_type) {
+    case "R":
+    case "r":
+        if($invoice_subtotal < 100) {
+            $discount_percent = .0;
+        } else if($invoice_subtotal >= 100 && $invoice_subtotal < 250) {
+            $discount_percent = .1;
+        } else if($invoice_subtotal >= 250 && $invoice_subtotal < 500) {
+            $discount_percent = .25;
+        } else if($invoice_subtotal >= 500) {
+            $discount_percent = .3;
+        }
+        break;
+    case "C":
+    case "c":
         $discount_percent = .2;
-    } else {
-        $discount_percent = .3;
-    }
-} else {
-    $discount_percent = .0;
+        break;
+    case "T":
+    case "t":
+        if($invoice_subtotal < 500) {
+            $discount_percent = .4;
+        } else if($invoice_subtotal >= 500) {
+            $discount_percent = .5;
+        }
+        break;
+    default:
+        $discount_percent = .1;
+        break;
 }
 
 $discount_amount = $invoice_subtotal * $discount_percent;
